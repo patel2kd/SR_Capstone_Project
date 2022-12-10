4f694acda8dfdc90007ad1d3a8a496bd174d830e
Kadam Patel <59839409+patel2kd@users.noreply.github.com>
2022-12-10 23:32:57 +0530

    Delete add_new_medicine.dart

--- lib/screens/add_new_medicine/add_new_medicine.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-class AddNewMedicine extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Text("ADD MEDICINE"),);
-  }
-}
